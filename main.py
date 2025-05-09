b88449296e7b59eee7853b623c54817df64b7c4b
danil <danyapablo200909@gmail.com>
2025-05-09 20:31:39 +0300

    aasadasdasdasd

--- main.py
+++ main.py
@@ -1,19 +1,29 @@
+from pydantic import HttpUrl
+from fastapi import FastAPI, HTTPException, Query
 import httpx
 from bs4 import BeautifulSoup
+from typing import Dict, Any
+from bs4 import BeautifulSoup
 import asyncio
+import uvicorn
 
-async def get_url(url):
-    async with httpx.AsyncClient() as client:
-        response = await client.get(url)
-        if response.status_code == 200:
-            soup = BeautifulSoup(response.text, 'html.parser')
-            answer = soup.find_all("span", class_="the_invis")
-            return answer
-        return []
+app = FastAPI(title="Parse anime")
 
-url = "https://jut.su/anime/"
-headings = asyncio.run(get_url(url))
+@app.get("/parse/")
+async def parse_url(url: HttpUrl = Query(..., description="URL to parse")) -> Dict[str, Any]:
+    async with httpx.AsyncClient(timeout=10.0) as client:
+        try:
 
-for i, h in enumerate(headings, 1):
-    print(f"{i}. {h.text.strip()}")
+            response = await client.get(str(url))
+            response.raise_for_status()
+        except httpx.RequestError as exc:
+            raise HTTPException(status_code=400, detail=f"Request error for {url}: {str(exc)}")
+        except httpx.HTTPStatusError as exc:
+            raise HTTPException(status_code=exc.response.status_code,
+                                detail=f"Error response {exc.response.status_code} while requesting {url}")
+
+    soup = BeautifulSoup(response.text, "html.parser")
+
+if __name__ == "__main__":
+    uvicorn.run("main:app", port=8006, reload=True)
 
