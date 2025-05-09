3de944157b883df252c53cac7d491b1b505b63b1
Art112-122 <a.zalutskij@gmail.com>
2025-05-09 20:16:09 +0300

    хлеб

--- main.py
+++ main.py
@@ -7,12 +7,13 @@ async def get_url(url):
         response = await client.get(url)
         if response.status_code == 200:
             soup = BeautifulSoup(response.text, 'html.parser')
-            titles = soup.find_all("a", class_="storylink")
-            return titles
+            answer = soup.find_all("span", class_="the_invis")
+            return answer
         return []
 
-if __name__ == "__main__":
-    url = "https://news.ycombinator.com/"
-    titles = asyncio.run(get_url(url))
-    for i, title in enumerate(titles, 1):
-        print(f"{i}. {title.text}")
+url = "https://jut.su/anime/"
+headings = asyncio.run(get_url(url))
+
+for i, h in enumerate(headings, 1):
+    print(f"{i}. {h.text.strip()}")
+
