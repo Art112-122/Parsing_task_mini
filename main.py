7d8e660ad65246b9f0ebb40cdb9591ba0b7b32d6
Art112-122 <a.zalutskij@gmail.com>
2025-05-10 13:19:55 +0300

    хлеб

--- main.py
+++ main.py
@@ -1,28 +1,31 @@
-from pydantic import HttpUrl
 from fastapi import FastAPI, HTTPException, Query
 import httpx
+from typing import Dict, Any, Literal
 from bs4 import BeautifulSoup
-from typing import Dict, Any
-from bs4 import BeautifulSoup
-import asyncio
 import uvicorn
+from parsing import get_url, get_anime
 
 app = FastAPI(title="Parse anime")
 
+BASIC_URL = "https://jut.su/anime/"
+
+
 @app.get("/parse/")
-async def parse_url(url: HttpUrl = Query(..., description="URL to parse")) -> Dict[str, Any]:
-    async with httpx.AsyncClient(timeout=10.0) as client:
-        try:
+async def parse_url(url: str = Query(..., description="URL to parse"),
+                    _class: str = Query("", description="Find for class"),
+                    tag: str = Query("", description="Find for tag")):
+    answer = await get_url(url, _class, tag)
+    return answer
 
-            response = await client.get(str(url))
-            response.raise_for_status()
-        except httpx.RequestError as exc:
-            raise HTTPException(status_code=400, detail=f"Request error for {url}: {str(exc)}")
-        except httpx.HTTPStatusError as exc:
-            raise HTTPException(status_code=exc.response.status_code,
-                                detail=f"Error response {exc.response.status_code} while requesting {url}")
-
-    soup = BeautifulSoup(response.text, "html.parser")
 
 if __name__ == "__main__":
     uvicorn.run("main:app", port=8006, reload=True)
+
+
+@app.get("/anime/{category}")
+async def anime(
+        category: Literal[
+            "adventure", "action", "comedy", "everyday", "romance", "drama", "fantastic", "fantasy", "mystic", "detective", "psychology"]):
+    url = f"{BASIC_URL}{category}/"
+    answer = await get_anime(url=url)
+    return answer
--- parsing.py
+++ parsing.py
@@ -1,13 +1,35 @@
 import httpx
 from bs4 import BeautifulSoup
+from fastapi import HTTPException
 
+BASIC_URL = "https://jut.su"
 
-async def get_url(url):
+async def get_url(url, _class: str | None = None, tag: str | None = None):
     async with httpx.AsyncClient() as client:
         response = await client.get(url)
         if response.status_code == 200:
             soup = BeautifulSoup(response.text, 'html.parser')
-            answer = soup.find_all("span", class_="the_invis")
-            return answer
-        return []
+            if _class:
+                if tag:
+                    answer = soup.find_all(f"{tag}", class_=f"{_class}")
+                    return {"answer": [str(tag) for tag in answer]}
 
+                answer = soup.find_all(class_=f"{_class}")
+                return {"answer": [str(tag) for tag in answer]}
+            elif tag:
+                answer = soup.find_all(f"{tag}")
+                return {"answer": [str(tag) for tag in answer]}
+            answer = soup.find_all()
+            return {"answer": [str(tag) for tag in answer]}
+        raise HTTPException(status_code=response.status_code, detail=response.text)
+
+
+async def get_anime(url):
+    async with httpx.AsyncClient() as client:
+
+        response = await client.get(url)
+        if response.status_code == 200:
+            soup = BeautifulSoup(response.text, 'html.parser')
+            answer = soup.find_all("span", class_="the_invis")
+            return {"answer": [f"{i} {a.text}: {BASIC_URL}{a.find('a').get('href')}" for i, a in enumerate(answer, 1)]}
+        raise HTTPException(status_code=response.status_code, detail=response.text)
