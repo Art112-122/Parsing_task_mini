fd0c173f9bd6cc5198f276780601389209fa0ba8
Art112-122 <a.zalutskij@gmail.com>
2025-05-12 11:20:09 +0300

    хлеб

--- main.py
+++ main.py
@@ -1,15 +1,19 @@
 from fastapi import FastAPI, HTTPException, Query
 import uvicorn
 from typing import Literal
-from parsing2 import get_url, get_anime, get_anime_info
+from parsing import get_url, get_anime, get_anime_info
+
 app = FastAPI(title="Parse anime")
 BASIC_URL = "https://jut.su/anime/"
+
+
 @app.get("/parse/")
 async def parse_url(url: str = Query(..., description="URL to parse"),
                     _class: str = Query("", description="Find for class"),
                     tag: str = Query("", description="Find for tag")):
     answer = await get_url(url, _class, tag)
     return answer
+
 
 @app.get("/anime/{category}")
 async def anime(
@@ -19,15 +23,11 @@ async def anime(
     answer = await get_anime(url=url)
     return answer
 
+
 @app.get("/anime/info/")
 async def anime_info(title: str = Query(..., description="Название аниме")):
     answer = await get_anime_info(title)
     return answer
-
-
-
-
-
 
 
 if __name__ == "__main__":
--- parsing.py
+++ parsing.py
@@ -2,7 +2,8 @@ import httpx
 from bs4 import BeautifulSoup
 from fastapi import HTTPException
 
-BASIC_URL = "https://jut.su/name/"
+BASIC_URL = "https://jut.su"
+
 
 async def get_url(url, _class: str | None = None, tag: str | None = None):
     async with httpx.AsyncClient() as client:
@@ -11,16 +12,18 @@ async def get_url(url, _class: str | None = None, tag: str | None = None):
             soup = BeautifulSoup(response.text, 'html.parser')
             if _class:
                 if tag:
-                    answer = soup.find_all(tag, class_=_class)
+                    answer = soup.find_all(f"{tag}", class_=f"{_class}")
                     return {"answer": [str(tag) for tag in answer]}
-                answer = soup.find_all(class_=_class)
+
+                answer = soup.find_all(class_=f"{_class}")
                 return {"answer": [str(tag) for tag in answer]}
             elif tag:
-                answer = soup.find_all(tag)
+                answer = soup.find_all(f"{tag}")
                 return {"answer": [str(tag) for tag in answer]}
             answer = soup.find_all()
             return {"answer": [str(tag) for tag in answer]}
         raise HTTPException(status_code=response.status_code, detail=response.text)
+
 
 async def get_anime(url):
     async with httpx.AsyncClient() as client:
@@ -28,30 +31,25 @@ async def get_anime(url):
         if response.status_code == 200:
             soup = BeautifulSoup(response.text, 'html.parser')
             answer = soup.find_all("span", class_="the_invis")
-            return {
-                "answer": [
-                    f"{i} {a.text}: {BASIC_URL}{a.find('a').get('href')}"
-                    for i, a in enumerate(answer, 1)
-                ]
-            }
+            return {"answer": [f"{i} {a.text}: {BASIC_URL}{a.find('a').get('href')}" for i, a in enumerate(answer, 1)]}
         raise HTTPException(status_code=response.status_code, detail=response.text)
 
 
 async def get_anime_info(title: str):
-    url = f"https://jut.su/{title}/"
+    url = f"{BASIC_URL}/{title}/"
     async with httpx.AsyncClient() as client:
-        response = await client.get(url)
-        if response.status_code == 200:
-            soup = BeautifulSoup(response.text, 'html.parser')
+        try:
+            response = await client.get(url)
+            if response.status_code == 200:
+                soup = BeautifulSoup(response.text, 'html.parser')
 
-            # Находим все эпизоды
-            episodes = soup.find_all("a", class_="one_manga_page_link")
-            total = len(episodes)
+                episodes = soup.find_all(class_="video")
 
-            return {
-                "title": title,
-                "total_episodes": total
-            }
+                total = len(episodes)
 
-    raise HTTPException(status_code=response.status_code, detail=response.text)
-
+                return {
+                    "title": title,
+                    "total_episodes": total
+                }
+        except httpx.ConnectError as e:
+            raise HTTPException(status_code=400, detail=e)
